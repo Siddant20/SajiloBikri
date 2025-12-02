f5bee56ba02cbdbd003d55f6bef227196f17beb4
Siddant Shrestha <siddantshrestha20@gmail.com>
2025-12-02 21:02:51 +0545

    SV-migration-models

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,7 +55,12 @@ class User extends Authenticatable
         return Str::of($this->name)
             ->explode(' ')
             ->take(2)
-            ->map(fn ($word) => Str::substr($word, 0, 1))
+            ->map(fn($word) => Str::substr($word, 0, 1))
             ->implode('');
+    }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
     }
 }
